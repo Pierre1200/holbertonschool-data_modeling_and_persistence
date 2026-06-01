e7ce9a4248a6940bc9856b1a1e44e7fbdcaa7640
Pierre1200 <rouvellat.p@icloud.com>
2026-06-01 11:50:55 +0200

    fix: update Books table schema by removing NOT NULL from id and adding NOT NULL to genre

--- sql_crud/0-create_table.sql
+++ sql_crud/0-create_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE books (
-    id INTEGER PRIMARY KEY NOT NULL,
+    id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     author TEXT NOT NULL,
-    genre TEXT,
+    genre TEXT NOT NULL,
     price REAL NOT NULL,
     stock INTEGER NOT NULL,
     published_year INTEGER NOT NULL
