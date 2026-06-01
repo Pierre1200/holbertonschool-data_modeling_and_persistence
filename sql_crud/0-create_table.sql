9475750c37234a253322738f2a80592701654e3b
Pierre1200 <rouvellat.p@icloud.com>
2026-06-01 11:40:41 +0200

    create Books table and add README for SQL CRUD operations

--- sql_crud/0-create_table.sql
+++ sql_crud/0-create_table.sql
@@ -1,3 +1,9 @@
-SQL
-
-CREATE TABLE Books:
+CREATE TABLE Books (
+    id INTEGER PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    genre TEXT,
+    price REAL NOT NULL,
+    stock INTEGER NOT NULL,
+    published_year INTEGER NOT NULL
+);
