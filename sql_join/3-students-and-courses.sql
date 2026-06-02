a9e8524f749797f87005e66cb80bf9aed34df707
Pierre1200 <rouvellat.p@icloud.com>
2026-06-02 11:58:39 +0200

    fix: correct SQL query to properly reference tables and ensure accurate course title selection

--- sql_join/3-students-and-courses.sql
+++ sql_join/3-students-and-courses.sql
@@ -1,7 +1,7 @@
-SELECT name AS student_name, title AS course_title
-FROM student, courses
+SELECT name AS student_name, courses.title AS course_title
+FROM students
 INNER JOIN enrollments
 ON students.id = enrollments.student_id
 INNER JOIN courses
-ON enrollments.course_id = course.id
+ON enrollments.course_id = courses.id
 ORDER BY student_name ASC, course_title ASC;
